528fd8b17c41b0711bc4d1ed37ac69b3dc2bf56c
Pavel Berezkin <Berezkin.P@dns-shop.ru>
2022-02-01 10:11:03 +1000

    Добавил пример 001 - Простое использование таски

---
+++ N_001_TPL/N_001_TPL/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace N_001_TPL
+{
+    class Program
+    {
+        /// <summary>
+        /// Метод выполняемый асинхронно
+        /// </summary>
+        private static void MyTask()
+        {
+            int threadId = Thread.CurrentThread.ManagedThreadId;
+
+            Console.Write(Environment.NewLine);
+            Console.WriteLine($"MyTask: запуск в потоке # {threadId}");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(200);
+                Console.Write("+ ");
+            }
+            
+            Console.Write(Environment.NewLine);
+            Console.WriteLine($"MyTask: завершен в потоке # {threadId}");
+        }
+        
+        public static void Main()
+        {
+            int threadId = Thread.CurrentThread.ManagedThreadId;
+            
+            Console.WriteLine($"Main: запущен в потоке {threadId}");
+
+            Action action = new Action(MyTask);
+
+            Task task = new Task(action); // Создание экземпляра задачи
+            //task.Start();               // Запуск задачи на выполнение асинхронно 
+            
+            task.RunSynchronously();      // Запуск задачи на выполнение синхронно
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(". ");
+                Thread.Sleep(200);
+            }
+
+            Console.Write(Environment.NewLine);
+            Console.WriteLine($"Main: завершен в потоке {threadId}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ N_001_TPL/N_001_TPL/README.md
@@ -0,0 +1,9 @@
+Пример асинхронного выполнения
+
+![](img/Screenshot_1.png)
+
+Пример синхронного выполнения
+
+![](img/Screenshot_3.png)
+
+![](img/Screenshot_2.png)
