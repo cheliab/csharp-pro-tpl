a99adc94a4ef175beeccb6fa79b65113415fb97b
Berezkin Pavel <cheliabkns@gmail.com>
2022-02-05 15:45:22 +1000

    Добавил пример использования лямбда-выражений для выполнения задачи - 009

---
+++ N_009_Lambda/Program.cs
@@ -0,0 +1,35 @@
+﻿// Использование лямбда-выражения в качестве задачи
+
+using System.Globalization;
+
+namespace N_009_Lambda
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Основной поток запущен.");
+
+            // Использование лябда-оператора для опредения задачи
+            Task task = Task.Factory.StartNew(new Action(() =>
+                {
+                    for (int i = 0; i < 80; i++)
+                    {
+                        Thread.Sleep(20);
+                        Console.Write(".");
+                    }
+                }));
+
+            // Ждем завершения задачи
+            task.Wait();
+            
+            // Освобождение задачи
+            task.Dispose();
+            
+            Console.WriteLine("Основной поток завершен.");
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
