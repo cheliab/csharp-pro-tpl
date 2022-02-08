8de8839b5e07a2b81bc030b75171784033dc646c
Berezkin Pavel <cheliabkns@gmail.com>
2022-02-09 07:52:24 +1000

    Добавил пример вызова исключения внутри задачи - 012

---
+++ N_013_TPL_Exception/Program.cs
@@ -0,0 +1,50 @@
+﻿// Работа с исключениями
+
+namespace N_013_TPL_Exception;
+
+class Program
+{
+    /// <summary>
+    /// Метод с исключением
+    /// </summary>
+    private static void MyTask()
+    {
+        Console.WriteLine("Задача запущена.");
+
+        throw new Exception();
+        
+        Console.WriteLine("Задача завершена");
+    }
+    
+    public static void Main()
+    {
+        Console.WriteLine("Основной поток запущен.");
+
+        Task task = new Task(MyTask);
+
+        try
+        {
+            task.Start();
+            
+            // Для обработки исключения обязательно вызвать Wait ()
+            // task.Wait(); 
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Exception: {ex.GetType()}");
+            Console.WriteLine($"Message: {ex.Message}");
+
+            if (ex.InnerException != null)
+                Console.WriteLine($"Inner Exception: {ex.InnerException}");
+        }
+        finally
+        {
+            Console.WriteLine($"Статус задачи: {task.Status}");
+        }
+
+        Console.WriteLine("Основной поток завершен.");
+
+        // Delay
+        Console.ReadKey();
+    }
+}
---
+++ N_013_TPL_Exception/README.md
@@ -0,0 +1,5 @@
+﻿![](img/Screenshot_1.png)
+
+Если не ждать через Wait(), то исключение не ловится
+
+![](img/Screenshot_2.png)
