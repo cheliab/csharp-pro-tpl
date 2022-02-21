9d0340d6496e51b7e919a896c732e49ce547f69d
Berezkin Pavel <cheliabkns@gmail.com>
2022-02-22 00:19:14 +1000

    Пример использования параметров настроек для выбора варианта продолжения задачи

---
+++ N_014_TPL_ContinuationOptions/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace N_014_TPL_ContinuationOptions;
+
+class Propgram
+{
+    private static int MyTask()
+    {
+        byte result = 255;
+
+        checked // убрать коммент для вызова исключения 
+        {
+            result += 1;
+        }
+
+        return result;
+    }
+    
+    public static void Main()
+    {
+        Task<int> task = new Task<int>(MyTask);
+        Action<Task<int>> continuation;
+        
+        // срабатывает при убранном "checked"
+        continuation = t => Console.WriteLine($"Result: {t.Result}");
+        task.ContinueWith(continuation, TaskContinuationOptions.OnlyOnRanToCompletion);
+
+        // срабатывает при наличии "checked"
+        continuation = t => Console.WriteLine($"Inner Exception: {t.Exception.InnerException.Message}");
+        task.ContinueWith(continuation, TaskContinuationOptions.OnlyOnFaulted);
+
+        task.Start();
+
+        Console.ReadKey();
+    }
+}
---
+++ N_014_TPL_ContinuationOptions/README.md
@@ -0,0 +1,7 @@
+﻿![](img/Screenshot_1.png)
+
+![](img/Screenshot_2.png)
+
+![](img/Screenshot_3.png)
+
+![](img/Screenshot_4.png)
